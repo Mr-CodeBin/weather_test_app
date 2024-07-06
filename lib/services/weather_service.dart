6a6961c7d61717ee6d12f7fe76d4ab6ca2f9532f
Nikhil Singh <109690866+Mr-CodeBin@users.noreply.github.com>
2024-07-06 21:39:04 +0530

    Update weather_service.dart

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 import 'package:weather_test_app/models/weather_data_model.dart';
 
 class WeatherService {
-  final String apiKey = "013ed29ddf9ecf85e8814f0a37264bb5";
+  final String apiKey = "Enter Your API KEY from OpenWeatherAPI";
 
   Future<WeatherData> fetchWeatherData(String cityName) async {
     final response = await http.Client().get(
